50cc7d87b8bf67f553edc9b1e132c2cb9bc0fdfe
jaujau088 <jaujau088@gmail.com>
2022-06-02 20:16:20 +0200

    Add Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => (
   <nav className="Flex-Row Align-Center">
     <h1>Bookstore CMS</h1>
     <ul className="Flex-Row Align-Center">
-      <li>BOOKS</li>
-      <li>CATEGORIES</li>
+      <li>
+        <Link to="/books" exact="true">BOOKS</Link>
+      </li>
+      <li>
+        <Link to="/categories" exact="true">CATEGORIES</Link>
+      </li>
     </ul>
   </nav>
 );
