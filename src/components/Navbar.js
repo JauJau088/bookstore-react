c0a0c83debea762e21039221cc868f249cb33fc8
jaujau088 <jaujau088@gmail.com>
2022-06-10 15:33:27 +0200

    Fix the margins and paddings to pixel-perfectly match the Zeplin design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,18 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => (
-  <nav className="Flex-Row Align-Center">
-    <h1>Bookstore CMS</h1>
-    <ul className="Flex-Row Align-Center">
-      <li>
-        <Link to="/books" exact="true">BOOKS</Link>
-      </li>
-      <li>
-        <Link to="/categories" exact="true">CATEGORIES</Link>
-      </li>
-    </ul>
+  <nav className="Flex-Row Align-Center Just-Center">
+    <div className="Nav-Container Flex-Row Align-Center">
+      <h1>Bookstore CMS</h1>
+      <ul className="Flex-Row Align-Center">
+        <li>
+          <Link to="/books" exact="true">BOOKS</Link>
+        </li>
+        <li>
+          <Link to="/categories" exact="true">CATEGORIES</Link>
+        </li>
+      </ul>
+    </div>
   </nav>
 );
 
